3e2c848c1965d5119bee365b356683939ceb718d
Badasscoder <badasscoder@outlook.com>
2022-03-31 04:45:39 +0500

    deployed to vercel

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,15 +1,36 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBtX0zbq8OWFoczz-D55IEQ-zQxcmjZ6BU",
-  authDomain: "invoice-app-yt.firebaseapp.com",
-  projectId: "invoice-app-yt",
-  storageBucket: "invoice-app-yt.appspot.com",
-  messagingSenderId: "638958069840",
-  appId: "1:638958069840:web:2a359ba0cf4b8c7b648955",
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: "AIzaSyDkt_Byt22Un8llrI1Vyg9yPnVb9eP2py8",
+  authDomain: "invoiceapp-b86ea.firebaseapp.com",
+  projectId: "invoiceapp-b86ea",
+  storageBucket: "invoiceapp-b86ea.appspot.com",
+  messagingSenderId: "647767218800",
+  appId: "1:647767218800:web:dc651532dcc46d65a0f2b5",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-export default firebaseApp.firestore();
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
+
+firebase.firestore()
+  .enablePersistence()
+  .catch((err) => {
+    if (err.code === 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code === 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  });
+
+export default db;
